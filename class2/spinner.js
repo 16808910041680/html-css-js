88c0ea9d41504052dcc548657747faf9264ecff7
16808910041680 <forforfirefire@gmail.com>
2025-01-31 11:10:35 +1100

    hi

---
+++ class2/spinner.js
@@ -0,0 +1,23 @@
+var check1 = false;
+var check2 = false;
+var check3 = false;
+var check4 = false;
+var check5 = false;
+var check1 = false;
+var check1 = false;
+var check1 = false;
+var check1 = false;
+var check1 = false;
+var check1 = false;
+var check1 = false;
+var check1 = false;
+var check1 = false;
+var check1 = false;
+var check1 = false;
+var check1 = false;
+var check1 = false;
+var check1 = false;
+var check1 = false;
+function spinner1() {
+    if ()
+}
