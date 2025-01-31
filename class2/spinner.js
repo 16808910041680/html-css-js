49d83fd03c08fc465d2efcca634fc92630a3e5c8
16808910041680 <forforfirefire@gmail.com>
2025-01-31 11:20:05 +1100

    hi

--- class2/spinner.js
+++ class2/spinner.js
@@ -3,21 +3,240 @@ var check2 = false;
 var check3 = false;
 var check4 = false;
 var check5 = false;
-var check1 = false;
-var check1 = false;
-var check1 = false;
-var check1 = false;
-var check1 = false;
-var check1 = false;
-var check1 = false;
-var check1 = false;
-var check1 = false;
-var check1 = false;
-var check1 = false;
-var check1 = false;
-var check1 = false;
-var check1 = false;
-var check1 = false;
+var check6 = false;
+var check7 = false;
+var check8 = false;
+var check9 = false;
+var check10 = false;
+var check11 = false;
+var check12 = false;
+var check13 = false;
+var check14 = false;
+var check15 = false;
+var check16 = false;
+var check17 = false;
+var check18 = false;
+var check19 = false;
+var check20 = false;
 function spinner1() {
-    if ()
+    if (check1 == false) {
+        check1 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert ("hi! This is a disclaimer.")
+    }
+    else {
+        check1 == false;
+    }
+}
+function spinner2() {
+    if (check2 == false) {
+        check2 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check2 == false;
+    }
+}
+function spinner3() {
+    if (check3 == false) {
+        check3 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check3 == false;
+    }
+}
+function spinner4() {
+    if (check4 == false) {
+        check4 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check4 == false;
+    }
+}
+function spinner5() {
+    if (check5 == false) {
+        check5 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check5 == false;
+    }
+}
+
+function spinner6() {
+    if (check6 == false) {
+        check6 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check6 == false;
+    }
+}
+function spinner7() {
+    if (check7 == false) {
+        check7 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check7 == false;
+    }
+}
+function spinner8() {
+    if (check8 == false) {
+        check8 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check8 == false;
+    }
+}
+function spinner9() {
+    if (check9 == false) {
+        check9 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check9 == false;
+    }
+}
+function spinner10() {
+    if (check10 == false) {
+        check10 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check10 == false;
+    }
+}
+function spinner11() {
+    if (check11 == false) {
+        check11 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check11 == false;
+    }
+}
+function spinner12() {
+    if (check12 == false) {
+        check12 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check12 == false;
+    }
+}
+function spinner13() {
+    if (check13 == false) {
+        check13 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check13 == false;
+    }
+}
+function spinner14() {
+    if (check14 == false) {
+        check14 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check14 == false;
+    }
+}
+function spinner15() {
+    if (check15 == false) {
+        check15 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check15 == false;
+    }
+}
+
+function spinner16() {
+    if (check16 == false) {
+        check16 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check16 == false;
+    }
+}
+function spinner17() {
+    if (check17 == false) {
+        check17 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check17 == false;
+    }
+}
+function spinner18() {
+    if (check18 == false) {
+        check18 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check18 == false;
+    }
+}
+function spinner19() {
+    if (check19 == false) {
+        check19 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check19 == false;
+    }
+}
+function spinner20() {
+    if (check20 == false) {
+        check20 == true;
+    }
+    else if (check1 == true & check2 == true & check3 == true & check4 == true & check5 == true & check6 == true & check7 == true & check8 == true & check9 == true & check10 == true & check11 == true & check12 == true & check13 == true & check14 == true & check15 == true & check16 == true & check17 == true & check18 == true & check19 == true & check20 == true) {
+        alert("hi! This is a disclaimer.")
+    }
+    else {
+        check20 == false;
+    }
 }
