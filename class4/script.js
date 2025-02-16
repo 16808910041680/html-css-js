7dae7ac5e8e633a3129b996c4a49047951443acb
16808910041680 <forforfirefire@gmail.com>
2025-02-16 11:16:30 +1100

    h

--- class4/script.js
+++ class4/script.js
@@ -1,3 +1,6 @@
 function hi() {
     alert("Bo'oh'o'wa'er")
 }
+function vote() {
+    alert("Whyyyyyy? Bananas, as shown in the images, (carousel) are very nutritional and provide great health benifits. Bananas make you happier, the give your muscles strength, and are good for cramps. Ok, I'm done now. That's enough for one paragraph. Though with my fullest extent of my fantastic volcabulary, I provide extra, crucial words to this paragraph using the art of wasting words. Ok, I'm actually done now.")
+}
